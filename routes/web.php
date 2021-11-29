06d633b488eccaea5e8fc8920191a66cbb5f41f3
Abid Al Wassie <abidalwassie@outlook.com>
2021-11-29 23:08:55 +0600

    updated project

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'PagesController@index');
 
 Route::get('/about', function () {
     return view('about');
+});
+
+Route::get('/users/{id}', function ($id) {
+    return 'This is user ' . $id;
 });
